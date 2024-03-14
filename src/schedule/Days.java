727282d6270cc88cca07baedec13446e8b69d02e
bshainman <shainman.b@northeastern.edu>
2024-03-14 03:17:12 -0400

    Fixed mispelling.

--- src/schedule/Days.java
+++ src/schedule/Days.java
@@ -1,7 +1,7 @@
 package schedule;
 
 /**
- * RRepresentation of the days of the week as a concrete class.
+ * Representation of the days of the week as a concrete class.
  */
 public enum Days {
   MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
